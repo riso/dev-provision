145a4eb9115246f87aadd0b1d7ad9ed8cb5d5acf
Valerio Ponte <valerio.ponte@gmail.com>
2015-03-08 16:45:29 +0100

    Implement tmux module, symlink tmux config files

--- puppet/modules/tmux/manifests/init.pp
+++ puppet/modules/tmux/manifests/init.pp
@@ -1,1 +1,20 @@
-class tmux {}
+class tmux {
+
+  # setup .tmux.conf
+  file { "$env_pwd/.tmux.conf":
+    ensure  => link,
+    target  => "$utils::base::dotfiles/tmux.conf",
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+    require => File["$utils::base::dotfiles"],
+  }
+
+  # setup .tmux
+  file { "$env_pwd/.tmux":
+    ensure  => link,
+    target  => "$utils::base::dotfiles/tmux",
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+    require => File["$utils::base::dotfiles"],
+  }
+}
