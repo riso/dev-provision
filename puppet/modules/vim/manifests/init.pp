e35a6c307ead16c0256b2f79ea21b50cc0cb1854
Valerio Ponte <valerio.ponte@gmail.com>
2015-03-08 17:07:31 +0100

    Implement vim module, symlink vim config file and dirs

--- puppet/modules/vim/manifests/init.pp
+++ puppet/modules/vim/manifests/init.pp
@@ -1,3 +1,38 @@
 class vim {
 
+  $vim = "$env_pwd/.vim"
+
+  # create .vim dir
+  file { "$vim":
+    ensure => directory,
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+  }
+
+  # setup .vimrc
+  file { "$env_pwd/.vimrc":
+    ensure  => link,
+    target  => "$utils::base::dotfiles/vimrc",
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+    require => File["$utils::base::dotfiles"],
+  }
+
+  # setup bundle dir
+  file { "$vim/bundle":
+    ensure  => link,
+    target  => "$utils::base::dotfiles/bundle",
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+    require => File["$utils::base::dotfiles"],
+  }
+
+  # setup autoload dir
+  file { "$vim/autoload":
+    ensure  => link,
+    target  => "$utils::base::dotfiles/autoload",
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+    require => File["$utils::base::dotfiles"],
+  }
 }
