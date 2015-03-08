ae1c474f9fac6e040074274b2872aec99a7b79fd
Valerio Ponte <valerio.ponte@gmail.com>
2015-03-08 12:55:52 +0100

    Add packages required for dev box

--- puppet/modules/utils/manifests/base.pp
+++ puppet/modules/utils/manifests/base.pp
@@ -1,6 +1,11 @@
 class utils::base {
-  if ! defined(Package['git'])      { package { 'git':      ensure => present } }
-  if ! defined(Package['vim'])      { package { 'vim':      ensure => present } }
-  if ! defined(Package['zsh'])      { package { 'zsh':      ensure => present } }
-  if ! defined(Package['tmux'])     { package { 'tmux':     ensure => present } }
+  if ! defined(Package['htop'])       { package { 'htop':       ensure => present } }
+  if ! defined(Package['iotop'])      { package { 'iotop':      ensure => present } }
+  if ! defined(Package['git'])        { package { 'git':        ensure => present } }
+  if ! defined(Package['vim'])        { package { 'vim':        ensure => present } }
+  if ! defined(Package['zsh'])        { package { 'zsh':        ensure => present } }
+  if ! defined(Package['tmux'])       { package { 'tmux':       ensure => present } }
+  if ! defined(Package['maven'])      { package { 'maven':      ensure => present } }
+  if ! defined(Package['npm'])        { package { 'npm':        ensure => present } }
+  if ! defined(Package['subversion']) { package { 'subversion': ensure => present } }
 }
