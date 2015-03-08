5ad6562051aded56e572bfd88aca6cb37c4fec1c
Valerio Ponte <valerio.ponte@gmail.com>
2015-03-08 16:16:06 +0100

    Download config file repo, setup .zshrc

--- puppet/modules/utils/manifests/base.pp
+++ puppet/modules/utils/manifests/base.pp
@@ -5,4 +5,23 @@ class utils::base {
   if ! defined(Package['vim'])        { package { 'vim':        ensure => present } }
   if ! defined(Package['zsh'])        { package { 'zsh':        ensure => present } }
   if ! defined(Package['tmux'])       { package { 'tmux':       ensure => present } }
+
+  $dotfiles = "$env_pwd/.dotfiles" 
+
+  # download config files
+  vcsrepo { "$dotfiles":
+    ensure   => present,
+    provider => git,
+    source   => "https://github.com/riso/dotfiles.git",
+  }
+
+  # change owner of config files
+  file { "$dotfiles": 
+    ensure  => directory,
+    recurse => true,
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+    require => Vcsrepo["$dotfiles"],
+  }
+
 }
--- puppet/modules/zsh/manifests/init.pp
+++ puppet/modules/zsh/manifests/init.pp
@@ -1,16 +1,13 @@
-include zsh
-
 class zsh {
 
-  require utils::base
-  
   $oh_my_zsh = "$env_pwd/.oh-my-zsh"
 
   # download oh-my-zsh
   vcsrepo { "$oh_my_zsh":
-    ensure   => present,
-    provider => git,
-    source   => "https://github.com/robbyrussell/oh-my-zsh.git",
+    ensure    => present,
+    provider  => git,
+    source    => "https://github.com/robbyrussell/oh-my-zsh.git",
+    require   => Package['git'],
   }
 
   # change owner of oh-my-zsh
@@ -24,10 +21,17 @@ class zsh {
 
   # change user default shell
   user { "$env_sudo_user":
-    ensure => present,
-    shell  => "/usr/bin/zsh",
-    require => Vcsrepo["$oh_my_zsh"],
+    ensure  => present,
+    shell   => "/usr/bin/zsh",
+    require => Package['zsh'],
   }
 
   # setup .zshrc
+  file { "$env_pwd/.zshrc":
+    ensure  => link,
+    target  => "$utils::base::dotfiles/zshrc",
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+    require => File["$utils::base::dotfiles"],
+  }
 }
