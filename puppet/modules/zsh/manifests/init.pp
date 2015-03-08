34f8065f10bdaac75cf20f8901ba4ee12c5a7fe8
Valerio Ponte <valerio.ponte@gmail.com>
2015-03-08 15:19:48 +0100

    Implement setup of oh-my-zsh

--- puppet/modules/zsh/manifests/init.pp
+++ puppet/modules/zsh/manifests/init.pp
@@ -2,4 +2,32 @@ include zsh
 
 class zsh {
 
+  require utils::base
+  
+  $oh_my_zsh = "$env_pwd/.oh-my-zsh"
+
+  # download oh-my-zsh
+  vcsrepo { "$oh_my_zsh":
+    ensure   => present,
+    provider => git,
+    source   => "https://github.com/robbyrussell/oh-my-zsh.git",
+  }
+
+  # change owner of oh-my-zsh
+  file { "$oh_my_zsh": 
+    ensure  => directory,
+    recurse => true,
+    owner   => "$env_sudo_user",
+    group   => "$env_sudo_user",
+    require => Vcsrepo["$oh_my_zsh"],
+  }
+
+  # change user default shell
+  user { "$env_sudo_user":
+    ensure => present,
+    shell  => "/usr/bin/zsh",
+    require => Vcsrepo["$oh_my_zsh"],
+  }
+
+  # setup .zshrc
 }
