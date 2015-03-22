966db3194890b9f06b2c157886063b852e84ff58
Valerio Ponte <valerio.ponte@gmail.com>
2015-03-22 15:42:49 +0100

    Install xclip, required by tmux-yank

--- puppet/modules/tmux/manifests/init.pp
+++ puppet/modules/tmux/manifests/init.pp
@@ -2,7 +2,8 @@ class tmux {
 
   require epel
 
-  if ! defined(Package['tmux'])       { package { 'tmux': ensure => present } }
+  if ! defined(Package['tmux'])       { package { 'tmux':   ensure => present } }
+  if ! defined(Package['xclip'])      { package { 'xclip':  ensure => present } }
 
   $terminfo_path = $operatingsystem ? {
     /(Fedora|CentOS)/ => '/usr/share/terminfo/s/screen-256color',
